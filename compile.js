24634cd2fcfb0ad69794dc53d02a839f3a1c054c
VitalikL <lim.vitaliy@gmail.com>
2016-10-10 15:44:14 -0400

    Add create.js (initial version) to ease up the process of adding new quarterly. Update compile.js to work with create.js and update resultant structure of quarterly lesson. Update API with last_modified and checksum Add new dependencies required for new API

--- compile.js
+++ compile.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var metaMarked =    require("meta-marked"),
     fs =            require("fs-extra"),
     yamljs =        require("yamljs"),
@@ -10,13 +11,21 @@ var SOURCE_PATH = "src/",
     DIST_PATH = "dist/",
     DIST_INFO_FILE = "info.json",
     DIST_COVER_FILE = "cover.png",
-    DIST_EXTENSION = "html",
-    DIST_S3 = "https://s3-us-west-2.amazonaws.com/com.cryart.sabbathschool";
+    DIST_EXTENSION = "json";
+
+var crypto = require("crypto");
+
+function createChecksum(str){
+  return crypto
+    .createHash("sha256")
+    .update(str, "utf8")
+    .digest("hex")
+}
 
 
 var processLang = function(path){
   var quarterlies = fs.readdirSync(path).reverse(),
-      lang_info = [];
+      lang_info = { "quarterlies": [] };
 
   for (var i = 0; i < quarterlies.length; i++){
     var quarterly_path = path + "/" + quarterlies[i];
@@ -31,34 +40,76 @@ var processLang = function(path){
     quarterly_info.lessons = [];
 
     fs.copySync(quarterly_path + "/" + SOURCE_COVER_FILE, quarterly_path.replace(SOURCE_PATH, DIST_PATH) + "/" + DIST_COVER_FILE);
-    quarterly_info.cover = DIST_S3 + "/" + quarterly_path.replace(SOURCE_PATH, "") + "/" + DIST_COVER_FILE;
+    quarterly_info.cover = quarterly_path.replace(SOURCE_PATH, "") + "/" + DIST_COVER_FILE;
 
     for (var j = 0; j < quarterly.length; j++){
-      var extension = quarterly[j].split(".").pop();
-      if (extension != SOURCE_EXTENSION) continue;
+      var lesson_dir = quarterly_path + "/" + quarterly[j];
+      if (!fs.lstatSync(lesson_dir).isDirectory()) continue;
 
-      var day_path = quarterly_path.replace(SOURCE_PATH, DIST_PATH) + "/" + quarterly[j].replace(SOURCE_EXTENSION, DIST_EXTENSION),
-          quarterly_md = metaMarked(fs.readFileSync(quarterly_path + "/" + quarterly[j], "utf-8"));
+      var lessons = fs.readdirSync(lesson_dir);
 
-      fswf(day_path, quarterly_md.html);
-      quarterly_md.meta.path = DIST_S3 + "/" + quarterly_path.replace(SOURCE_PATH, "") + "/" + quarterly[j].replace(SOURCE_EXTENSION, DIST_EXTENSION);
-      quarterly_info.lessons.push(quarterly_md.meta);
+      var lesson_md_info = yamljs.load(lesson_dir + "/" + SOURCE_INFO_FILE);
+      lesson_md_info.days = [];
+
+      for (var k = 0; k < lessons.length; k++){
+        var extension = lessons[k].split(".").pop();
+        if (extension != SOURCE_EXTENSION) continue;
+
+        var day_path = lesson_dir.replace(SOURCE_PATH, DIST_PATH) + "/" + lessons[k].replace(SOURCE_EXTENSION, DIST_EXTENSION),
+          lesson_md = metaMarked(fs.readFileSync(lesson_dir + "/" + lessons[k], "utf-8"));
+
+        lesson_md.meta.path = day_path.replace(DIST_PATH, "");
+        lesson_md_info.days.push(lesson_md.meta);
+
+        var lesson_contents = {};
+        lesson_contents = lesson_md.meta;
+        lesson_contents.html = lesson_md.html;
+
+        lesson_contents.checksum = createChecksum(JSON.stringify(lesson_contents));
+        lesson_contents.last_modified = Date.now();
+        fswf(day_path, JSON.stringify(lesson_contents));
+        delete lesson_contents.html;
+
+      }
+
+      lesson_md_info.checksum = createChecksum(JSON.stringify(lesson_md_info));
+      lesson_md_info.last_modified = Date.now();
+      fswf(quarterly_path.replace(SOURCE_PATH, DIST_PATH) + "/" + "/" + quarterly[j] + "/" + DIST_INFO_FILE, JSON.stringify(lesson_md_info));
+      delete lesson_md_info.days;
+      lesson_md_info.path = quarterly_path.replace(SOURCE_PATH, "") + "/" + quarterly[j] + "/" + DIST_INFO_FILE;
+      quarterly_info.lessons.push(lesson_md_info);
     }
 
+    quarterly_info.path = quarterly_path.replace(SOURCE_PATH, "") + "/" + DIST_INFO_FILE;
+
+    quarterly_info.checksum = createChecksum(JSON.stringify(quarterly_info));
+    quarterly_info.last_modified = Date.now();
     fswf(quarterly_path.replace(SOURCE_PATH, DIST_PATH) + "/" + DIST_INFO_FILE, JSON.stringify(quarterly_info));
     delete quarterly_info.lessons;
-    lang_info.push(quarterly_info);
-
+    lang_info.quarterlies.push(quarterly_info);
   }
 
+
+  lang_info.checksum = createChecksum(JSON.stringify(lang_info));
+  lang_info.last_modified = Date.now();
   fswf(path.replace(SOURCE_PATH, DIST_PATH) + "/" + DIST_INFO_FILE, JSON.stringify(lang_info));
 };
 
-var languages = fs.readdirSync(SOURCE_PATH);
+
+var languages = fs.readdirSync(SOURCE_PATH),
+    api_info = {
+      "languages": []
+    };
 
 for (var i = 0; i < languages.length; i++){
   if (!fs.lstatSync(SOURCE_PATH + languages[i]).isDirectory()) continue;
   processLang(SOURCE_PATH + languages[i]);
+  api_info.languages.push(languages[i]);
+
 }
 
+
+api_info.checksum = createChecksum(JSON.stringify(api_info));
+api_info.last_modified = Date.now();
+fswf(DIST_PATH+ "/" + DIST_INFO_FILE, JSON.stringify(api_info));
 
