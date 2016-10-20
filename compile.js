b886906189ff9f1f50f1675c09278228fc31a4e8
VitalikL <lim.vitaliy@gmail.com>
2016-10-19 23:03:22 -0400

    Firebase deployment

--- compile.js
+++ compile.js
@@ -4,10 +4,25 @@
  * Created by vitalik on 16-10-15.
  */
 
-var metaMarked =    require("meta-marked"),
+var exec = require('child_process').execSync,
+    metaMarked =    require("meta-marked"),
     fs =            require("fs-extra"),
     yamljs =        require("yamljs"),
     fswf =          require("safe-write-file");
+
+var firebase = require("firebase"),
+    async = require("async");
+
+firebase.initializeApp({
+  databaseURL: "https://blistering-inferno-8720.firebaseio.com",
+  serviceAccount: "deploy-creds.json",
+  databaseAuthVariableOverride: {
+    uid: "deploy"
+  }
+});
+
+var db = firebase.database(),
+    firebaseDeploymentTasks = [];
 
 var API_HOST = "http://com.cryart.sabbathschool.s3-website-us-west-2.amazonaws.com/api/",
     API_VERSION = "v1",
@@ -15,7 +30,22 @@ var API_HOST = "http://com.cryart.sabbathschool.s3-website-us-west-2.amazonaws.c
     SOURCE_INFO_FILE = "info.yml",
     SOURCE_COVER_FILE = "cover.png",
     SOURCE_EXTENSION = "md",
-    DIST_DIR = "dist/api/" + API_VERSION + "/";
+    DIST_DIR = "dist/api/" + API_VERSION + "/",
+    FIREBASE_DATABASE_LANGUAGES = "languages",
+    FIREBASE_DATABASE_QUARTERLIES = "quarterlies",
+    FIREBASE_DATABASE_QUARTERLY_INFO = "quarterly-info",
+    FIREBASE_DATABASE_LESSONS = "lessons",
+    FIREBASE_DATABASE_LESSON_INFO = "lesson-info",
+    FIREBASE_DATABASE_DAYS = "days",
+    FIREBASE_DATABASE_READ = "reads";
+
+
+var lastModified = process.argv.slice(2);
+
+var changeCheck = function(path){
+  if (!lastModified) return false;
+  return exec('git log --since="'+lastModified+'" '+path).toString().length>0;
+};
 
 var create_languages_api = function(){
   /**
@@ -24,12 +54,18 @@ var create_languages_api = function(){
    * /api/v1/languages
    */
   var languages = [],
-      _languages = fs.readdirSync(SOURCE_DIR)
+      _languages = fs.readdirSync(SOURCE_DIR);
 
   for (var i = 0; i < _languages.length; i++){
     if (!fs.lstatSync(SOURCE_DIR + _languages[i]).isDirectory()) continue;
     languages.push(yamljs.load(SOURCE_DIR + "/" + _languages[i] + "/" + SOURCE_INFO_FILE));
   }
+
+  firebaseDeploymentTasks.push(function(cb){
+    db.ref(FIREBASE_DATABASE_LANGUAGES).set(languages, function(e){
+      cb(false, true);
+    });
+  });
 
   fswf(DIST_DIR + "/languages/index.json", JSON.stringify(languages));
   return languages;
@@ -63,11 +99,28 @@ var create_days_api = function(language, quarterly, lesson){
     read.content = _read.html;
     read.verses = [];
 
+    days.push(day);
+
+    if (!changeCheck(WORKING_DIR + "/" + _days[i])) continue;
+
     fswf(DIST_DIR + language + "/quarterlies/" + quarterly + "/lessons/" + lesson + "/days/" + _day + "/index.json", JSON.stringify(day));
     fswf(DIST_DIR + language + "/quarterlies/" + quarterly + "/lessons/" + lesson + "/days/" + _day + "/read/index.json", JSON.stringify(read));
     //fswf(DIST_DIR + language + "/quarterlies/" + quarterly + "/lessons/" + lesson + "/days/" + _day + "/read/index.html", read.content);
-    days.push(day);
+
+    (function(child, data){
+      firebaseDeploymentTasks.push(function(cb){
+        db.ref(FIREBASE_DATABASE_READ).child(child).set(data, function(e){
+          cb(false, true);
+        });
+      })
+    })(day.index, read);
   }
+
+  firebaseDeploymentTasks.push(function(cb){
+    db.ref(FIREBASE_DATABASE_DAYS).child(language + "-" + quarterly + "-" + lesson).set(days, function(e){
+      cb(false, true);
+    });
+  });
 
   fswf(DIST_DIR + language + "/quarterlies/" + quarterly + "/lessons/" + lesson + "/days/index.json", JSON.stringify(days));
   return days;
@@ -88,14 +141,31 @@ var create_lessons_api = function(language, quarterly){
     var lesson = {};
     lesson.lesson = yamljs.load(WORKING_DIR + "/" + _lessons[i] + "/" + SOURCE_INFO_FILE);
     lesson.lesson.id = _lessons[i];
-    lesson.lesson.index = language + "/" + quarterly + "/" + _lessons[i];
+    lesson.lesson.index = language + "-" + quarterly + "-" + _lessons[i];
     lesson.lesson.path = language + "/quarterlies/" + quarterly + "/lessons/" + _lessons[i];
     lesson.lesson.full_path = API_HOST + API_VERSION + "/" + language + "/quarterlies/" + quarterly + "/lessons/" + _lessons[i];
     lesson.days = create_days_api(language, quarterly, _lessons[i]);
 
-    fswf(DIST_DIR + language + "/quarterlies/" + quarterly + "/lessons/" + _lessons[i] + "/index.json", JSON.stringify(lesson));
     lessons.push(lesson.lesson);
+
+    if (!changeCheck(WORKING_DIR + "/" + _lessons[i])) continue;
+
+    fswf(DIST_DIR + language + "/quarterlies/" + quarterly + "/lessons/" + _lessons[i] + "/index.json", JSON.stringify(lesson));
+
+    (function(child, data){
+      firebaseDeploymentTasks.push(function(cb){
+        db.ref(FIREBASE_DATABASE_LESSON_INFO).child(child).set(data, function(e){
+          cb(false, true);
+        });
+      });
+    })(lesson.lesson.index, lesson);
   }
+
+  firebaseDeploymentTasks.push(function(cb){
+    db.ref(FIREBASE_DATABASE_LESSONS).child(language + "-" + quarterly).set(lessons, function(e){
+      cb(false, true);
+    });
+  });
 
   fswf(DIST_DIR + language + "/quarterlies/" + quarterly + "/lessons/index.json", JSON.stringify(lessons));
   return lessons;
@@ -110,6 +180,8 @@ var create_quarterlies_api = function(language){
       quarterlies = [],
       _quarterlies = fs.readdirSync(WORKING_DIR).reverse();
 
+
+
   for (var i = 0; i < _quarterlies.length; i++){
     if (!fs.lstatSync(WORKING_DIR + "/" + _quarterlies[i]).isDirectory()) continue;
     fs.copySync(WORKING_DIR + "/" + _quarterlies[i] + "/" + SOURCE_COVER_FILE, DIST_DIR + language + "/quarterlies/" + _quarterlies[i] + "/" + SOURCE_COVER_FILE);
@@ -118,16 +190,33 @@ var create_quarterlies_api = function(language){
     quarterly.quarterly = yamljs.load(WORKING_DIR + "/" + _quarterlies[i] + "/" + SOURCE_INFO_FILE);
     quarterly.quarterly.lang = language;
     quarterly.quarterly.id = _quarterlies[i];
-    quarterly.quarterly.index = language + "/" + _quarterlies[i];
+    quarterly.quarterly.index = language + "-" + _quarterlies[i];
     quarterly.quarterly.path = language + "/quarterlies/" + _quarterlies[i];
     quarterly.quarterly.full_path = API_HOST + API_VERSION + "/" + language + "/quarterlies/" + _quarterlies[i];
     quarterly.quarterly.cover = API_HOST + API_VERSION + "/" + quarterly.quarterly.path + "/" + SOURCE_COVER_FILE;
 
     quarterly.lessons = create_lessons_api(language, _quarterlies[i]);
 
-    fswf(DIST_DIR + language + "/quarterlies/" + _quarterlies[i] + "/index.json", JSON.stringify(quarterly));
     quarterlies.push(quarterly.quarterly);
+
+    if (!changeCheck(WORKING_DIR + "/" + _quarterlies[i])) continue;
+
+    fswf(DIST_DIR + language + "/quarterlies/" + _quarterlies[i] + "/index.json", JSON.stringify(quarterly));
+
+    (function(child, data){
+      firebaseDeploymentTasks.push(function(cb){
+        db.ref(FIREBASE_DATABASE_QUARTERLY_INFO).child(child).set(data, function(e){
+          cb(false, true);
+        });
+      });
+    })(quarterly.quarterly.index, quarterly);
   }
+
+  firebaseDeploymentTasks.push(function(cb){
+    db.ref(FIREBASE_DATABASE_QUARTERLIES).child(language).set(quarterlies, function(e){
+      cb(false, true);
+    });
+  });
 
   fswf(DIST_DIR + language + "/quarterlies/index.json", JSON.stringify(quarterlies));
   return quarterlies;
@@ -138,3 +227,12 @@ var languages = create_languages_api();
 for (var i = 0; i < languages.length; i++){
   create_quarterlies_api(languages[i].code);
 }
+
+// Not syncing with Firebase if not run with argument. Argument is commit range
+if (lastModified){
+  async.series(firebaseDeploymentTasks,
+  function(err, results) {
+    db.goOffline();
+  });
+}
+
