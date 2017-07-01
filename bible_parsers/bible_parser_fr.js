25cf04f37d8e6ed7f926d9e05d2be43ff1d9c717
VitalikL <lim.vitaliy@gmail.com>
2017-07-01 14:43:55 -0400

    Update French Bible verse parser to include `et` format. Ex: John 1:1 et 2:1

--- bible_parsers/bible_parser_fr.js
+++ bible_parsers/bible_parser_fr.js
@@ -61,8 +61,8 @@ function scrape(verseKey, verseFind, version, cb){
 
 function parse_fr(read, callback){
   var bible_books = "Gn|Gen|Ex|Lev|Nom|Nb|Deut|Jos|Jg|Jug|Rt|1 ?Sam|2 ?Sam|1 ?R|2 ?R|1 ?Ch|2 ?Ch|1 ?Chron|2 ?Chron|Esd|Ne|Neh|Est|Jb|Ps|Pr|Prov|Ec|Isa|Esa|Jr|Jer|La|Ezdr|Ez|Da|Dan|Os|Jl|Am|Ab|Jon|Mic|Na|Hab|So|Ag|Zach|Ma|Mal|Mt|Mat|Matt|Mc|Lc|Jn|John|Acts|Act|Ac|Rom|1 ?Cor|2 ?Cor|Gal|Eph|Ph|Phil|Col|1 ?Th|2 ?Th|1 ?Tim|1 Thess|2 Thess|2 ?Tim|Tt|Phm|He|Hé|Héb|Heb|Jc|1 ?Pie|2 ?Pie|1 ?Jn|2 ?Jn|3 ?Jn|Jd|Ap|Apo|Apoc|Genèse|Exode|Lévitique|Nombres|Deutéronome|Josué|Juges|Ruth|1 ?Samuel|2 ?Samuel|1 ?Rois|2 ?Rois|1 ?Chroniques|2 ?Chroniques|Esdras|Néhémie|Esther|Job|Psaume|Psaumes|Proverbes|Proverbe|Ecclésiaste|Cantique des cantiques|Esaïe|Ésaïe|Jérémie|Lamentations|Ézéchiel|Daniel|Osée|Joël|Amos|Abdias|Jonas|Michée|Nahum|Habakuk|Sophonie|Aggée|Zacharie|Malachie|Matthieu|Marc|Luc|Jean|Actes|Romains|1 ?Corinthiens|2 ?Corinthiens|Galates|Éphésiens|Philippiens|Colossiens|1 ?Thessaloniciens|2 ?Thessaloniciens|1 ?Timothée|2 ?Timothée|Tite|Philémon|Hébreux|Jacques|1 ?Pierre|2 ?Pierre|1 ?Jean|2 ?Jean|3 ?Jean|Jude|Apocalypse";
-  var bible_regexp = new RegExp("(("+bible_books+")\\.?\\ ([0-9\\.;,: \\-\\–](?!"+bible_books+"))+)", "g");
-  var bible_book_regexp = new RegExp("("+bible_books+")\\.?", "g");
+  var bible_regexp = new RegExp("(("+bible_books+")\\.?\\ ([0-9\\.;,: \\-\\–](\ et\ )?(?!"+bible_books+"))+)", "g");
+  var bible_book_regexp = new RegExp("("+bible_books+")\\.?", "ig");
 
   var verses = read.match(bible_regexp),
     verses_parsed = [];
@@ -72,6 +72,12 @@ function parse_fr(read, callback){
   if (verses){
     for (var i = 0; i < verses.length; i++){
       var verse = customTrim(verses[i], " ;,()<>.:-");
+
+        if (verse.indexOf(" et ") > 0){
+            var new_verse = verse.replace(" et ", ", ");
+            read = read.replace(new RegExp(verse, "ig"), new_verse);
+            verse = new_verse;
+        }
 
       var french_verse_replacement = [
         {"s": "Esa.", "r": "Ésaïe"},
