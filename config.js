046b214643276464b8968e1cb875c848b1fae255
VitalikL <lim.vitaliy@gmail.com>
2018-03-30 17:14:40 -0400

    Updating compilation process - combining all three deployments in one script Making the compilation process faster and smarter Taking the Bible configuration out of compilation script Updating dependencies

---
+++ config.js
@@ -0,0 +1,104 @@
+var BIBLE_PARSER_CONFIG = {
+  "ar": [
+    "svd"
+  ],
+  "bg": [
+    "bg1940"
+  ],
+
+  "cs": [
+    "kralicka-1613"
+  ],
+
+  "da": [
+    "bph",
+    "dn1933"
+  ],
+
+  "de": [
+    "luth1545",
+    "luth1912"
+  ],
+
+  "en": [
+    "nasb",
+    "nkjv",
+    "kjv"
+  ],
+
+  "es": [
+    "rvr1960"
+  ],
+
+  "fa": [
+    "opv-1896"
+  ],
+
+  "fr": [
+    "lsg"
+  ],
+
+  "he": [
+    "em-1865"
+  ],
+
+  "ja": [
+    "kougo-yaku",
+    "jlb"
+  ],
+
+  "pt": [
+    "arc",
+    "nvi-pt"
+  ],
+
+  "in": [
+    "alkitab"
+  ],
+
+  "ko": [
+    "krv"
+  ],
+
+  "mk": [
+    "MKB"
+  ],
+
+  "ms": [
+    "alkitab"
+  ],
+
+  "ne": [
+    "erv"
+  ],
+
+  "no": [
+    "nb-bibelen-1930"
+  ],
+
+  "ro": [
+    "rmnn"
+  ],
+
+  "ru": [
+    "rusv"
+  ],
+
+  "sr": [
+    "biblija"
+  ],
+
+  "tr": [
+    "kitab"
+  ],
+
+  "uk": [
+    "ukr"
+  ],
+
+  "zh": [
+    "cuvs"
+  ]
+};
+
+module.exports = BIBLE_PARSER_CONFIG;
